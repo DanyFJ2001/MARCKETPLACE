fb78b6afa4fb9e37c57a86ad11297a3f68f6eab0
DanyFJ2001 <danyjoel345@gmail.com>
2025-02-13 18:41:58 -0500

    pr

---
+++ src/main/java/com/itsqmet/app_hotel/Seguridad/AutorizUsuario.java
@@ -0,0 +1,61 @@
+package com.itsqmet.app_hotel.Seguridad;
+
+import com.itsqmet.app_hotel.Servicio.UserDetailsServices;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class AutorizUsuario {
+
+    @Autowired
+    private UserDetailsServices userDetailsService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/","formularioAdmin","/formularioProveedor", "/formularioCliente", "/login", "/formularioLogin", "/formularioPrestaciones")
+                        .permitAll() // Rutas accesibles sin autenticación
+                        .requestMatchers("/css/**", "/js/**", "/images/**").permitAll() // Permitir acceso a archivos estáticos (CSS, JS, imágenes)
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/vistaCliente").hasRole("CLIENTE")
+                        .requestMatchers("/vistaProveedor").hasRole("PROVEEDOR")
+                        .anyRequest().authenticated()  // Cualquier otra ruta requiere autenticación
+                )
+                .formLogin(form -> form
+                        .loginPage("/formularioLogin")
+                        .permitAll()
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/postLogin", true)
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/formularioLogin?logout")
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID")
+                        .permitAll()
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+}
